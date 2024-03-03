91065a2dffefbcb8f1cff6b7755840af172780dc
thekarimovmurad <thekarimovmurad@gmail.com>
2024-03-03 23:16:10 +0100

    Add project files.

---
+++ Sort_Algorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Sort_Algorithms;
+using System.Collections;
+
+int[] intArray = { 5, 3, 8,12, 12,54, 4, 2, 7, 1 };
+string[] stringArray = { "adas", "dsad","abhgf","hfgbn" };
+QuickSort<int>.Sort(intArray);
+
+foreach (int num in intArray)
+{
+    Console.Write(num + " ");
+}
+QuickSort<string>.Sort(stringArray);
+
+foreach (string fruit in stringArray)
+{
+    Console.Write(fruit + " ");
+}
---
+++ Sort_Algorithms/QuickSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Algorithms
+{
+    public class QuickSort<T> where T : IComparable
+    {
+        public static void Sort(T[] array)
+        {
+            if (array == null || array.Length == 0)
+                return;
+
+            QuickSortAlgorithm(array, 0, array.Length - 1);
+        }
+
+        private static void QuickSortAlgorithm(T[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(array, left, right);
+                QuickSortAlgorithm(array, left, pivotIndex - 1);
+                QuickSortAlgorithm(array, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(T[] array, int left, int right)
+        {
+            T pivot = array[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j].CompareTo(pivot) < 0)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, right);
+            return i + 1;
+        }
+
+        private static void Swap(T[] array, int i, int j)
+        {
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+}
